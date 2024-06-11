95987440b85b07194f2f8bb79b89f21ec6a0627f
RadhikaYerramsetty <gantamohan.556@gmail.com>
2024-06-11 18:16:09 +0530

    update

--- server.js
+++ server.js
@@ -276,14 +276,14 @@ app.get('/attendance', verifyToken, async (req, res) => {
 });
 
 
-app.get('/attendance/:userId', verifyToken, async (req, res) => {
+app.get('/attendance/:userId', async (req, res) => {
   try {
     const { userId } = req.params; // Extract userId from the route parameters
-    const userLog = await UserLog.findOne({ userId }).populate('userId', 'username');
+    const userLog = await UserLog.find({ userId });
     if (!userLog) {
       return res.status(404).send('User log not found');
     }
-    res.json({ loginTime: userLog.loginTime });
+    res.json({userLog });
   } catch (error) {
     res.status(500).send('Error fetching attendance data');
   }
